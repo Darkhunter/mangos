200d22c32e0eccf33438dba0714edbfc1cb137b1
unknown <averm01s@.uisad.uis.edu>
2010-02-28 04:28:21 -0600

    cross-faction healing, buffing, resing, etc (except trade)

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -549,6 +549,7 @@ void World::LoadConfigSettings(bool reload)
     setConfig(CONFIG_BOOL_ALLOW_TWO_SIDE_INTERACTION_MAIL,    "AllowTwoSide.Interaction.Mail", false);
     setConfig(CONFIG_BOOL_ALLOW_TWO_SIDE_WHO_LIST,            "AllowTwoSide.WhoList", false);
     setConfig(CONFIG_BOOL_ALLOW_TWO_SIDE_ADD_FRIEND,          "AllowTwoSide.AddFriend", false);
+	setConfig(CONFIG_BOOL_ALLOW_TWO_SIDE_INTERACTION_FAKEFACTION, "AllowTwoSide.Interaction.FakeFactionForPlayers", false);
 
     setConfig(CONFIG_UINT32_STRICT_PLAYER_NAMES,  "StrictPlayerNames",  0);
     setConfig(CONFIG_UINT32_STRICT_CHARTER_NAMES, "StrictCharterNames", 0);
